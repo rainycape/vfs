6fa3142b47bc4e5d6225a082d802f622525e5b40
Alberto García Hierro <alberto@garciahierro.com>
2014-07-02 08:15:43 +0100

    Add buildfs subpackage
    Allows using a VFS as the filesystem for a go/build Context

---
+++ buildfs/buildfs.go
@@ -0,0 +1,44 @@
+// Package buildfs allows plugging a VFS into a go/build.Context.
+package buildfs
+
+import (
+	"go/build"
+	"io"
+	"path"
+	"path/filepath"
+	"strings"
+	"vfs"
+)
+
+const (
+	separator = "/"
+)
+
+// Setup configures a *build.Context to use the given VFS
+// as its filesystem.
+func Setup(ctx *build.Context, fs vfs.VFS) {
+	ctx.JoinPath = path.Join
+	ctx.SplitPathList = filepath.SplitList
+	ctx.IsAbsPath = func(p string) bool {
+		return p != "" && p[0] == '/'
+	}
+	ctx.IsDir = func(p string) bool {
+		stat, err := fs.Stat(p)
+		return err == nil && stat.IsDir()
+	}
+	ctx.HasSubdir = func(root, dir string) (string, bool) {
+		root = path.Clean(root)
+		if !strings.HasSuffix(root, separator) {
+			root += separator
+		}
+		dir = path.Clean(dir)
+		if !strings.HasPrefix(dir, root) {
+			return "", false
+		}
+		return dir[len(root):], true
+	}
+	ctx.ReadDir = fs.ReadDir
+	ctx.OpenFile = func(p string) (io.ReadCloser, error) {
+		return fs.Open(p)
+	}
+}
